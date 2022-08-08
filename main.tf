72fb3b7a3c9afb7db296fce9f7ee5538c158f447
rnitika <103918976+rnitika@users.noreply.github.com>
2022-08-08 11:12:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ variable "main_region" {
 
 provider "aws"{
 	region = var.main_region
-        access_key = "AKIAXRGVXBCCW2WUWVXE"
-	secret_key = "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"
+        access_key = "acess_key"
+	secret_key = "secret_key"
 }
 
 module "vpc" {
@@ -14,7 +14,7 @@ module "vpc" {
 }
 resource "aws_key_pair" "my-key"{
          key_name = "my-key"
-	 public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDsJgZBbWFIC77TGPqHvKZVhyHM57QNSigEazveTKyll+ZHi/4pOWwu29BruHupfyX3oLGQGPwa1jyEczefJ/sU7R/zUdbhbfw56erk5znZTwSlubWCjwCMiHLGOIGumVC6Z6Fjk2JpM334/ADslcUvSvmutyG/dkwWuJeOySh0A2WLR7iSmyDDGf35mIyj9s8oWqRbqOJFbMiEYgfQPPnkRNXsiX8c2gMa2jGuvCu9vGQ4vu4XKaIvtSYheGGT0E2JeYMeAS14DF6v+pcoXdMeEvJ3tc89jf3Mo/DK9PMkKXabgfCeKq2J5NjcE+WE6Pbi3D9J9VBTdg5PTnoOp6Ql root@ip-172-31-23-144.ec2.internal"
+	 public_key = "public_key"
 }
 
 resource "aws_instance" "my-instance" {
